c6aa1ef2a0af10e09171b525edce58c9eed0bfcc
Gbolahan Akande <geakande@gmail.com>
2024-08-21 16:02:29 +0100

    Basic cache module

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,19 @@
+#!usr/bin/env python3
+"""Basic cache module"""
+
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """BasicCache class"""
+
+    def put(self, key, item):
+        """Add an item in the cache"""
+        if key and item:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """Get an item by key"""
+        if key in self.cache_data:
+            return self.cache_data[key]
+        return None
