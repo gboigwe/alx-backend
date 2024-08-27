ba8a68130d2a58227c2e5249162f9dd1411f3113
Gbolahan Akande <geakande@gmail.com>
2024-08-28 00:56:12 +0100

    Added 0-index.html file to run flask

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -7,6 +7,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__, template_folder="templates")
 
+
 @app.route("/", methods=["GET"], strict_slashes=False)
 def hello_world() -> str:
     """Rendering the template for the flask"""
