528d272aa79268035a96623804127ab9a98d42ef
Gbolahan Akande <geakande@gmail.com>
2024-08-28 00:53:25 +0100

    Updated python file to run flask

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -8,10 +8,10 @@ from flask import Flask, render_template
 app = Flask(__name__, template_folder="templates")
 
 @app.route("/", methods=["GET"], strict_slashes=False)
-def hello_world():
+def hello_world() -> str:
     """Rendering the template for the flask"""
     return tender_template("0-index.html")
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+    app.run()
