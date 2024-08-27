c32e76e50031ac8ebc0d4020ae44027f9864d26e
Gbolahan Akande <geakande@gmail.com>
2024-08-28 00:50:21 +0100

    Updated python file to run flask

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python3
+"""Creating a basic flask app"""
 
 
-from flask import Flask
+from flask import Flask, render_template
 
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates")
 
-@app.route("/")
+@app.route("/", method=["GET"], strict_slashes=False)
+def hello_world():
+    """Rendering the template for the flask"""
+    return tender_template("0-index.html")
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
